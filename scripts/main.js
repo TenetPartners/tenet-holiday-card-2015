2ad6c399c4cef9f9afffbd2560fa5704a894b920
Ernie Bello <ernie@ern.me>
2015-12-03 17:55:05 -0500

    polyfill for IE

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AnonymousSurveyApp from './components/AnonymousSurveyApp'
 import '../lib/picturefill';
+import '../lib/polyfills';
 
 ReactDOM.render(<AnonymousSurveyApp/>, document.querySelector('#main'));
