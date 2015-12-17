7bc28e02e71a6ca06e766c81fa7c2c7215e46639
Ernie Bello <ernie@ern.me>
2015-12-17 13:28:51 -0500

    fixed gulp clean

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,3 +1,3 @@
 module.exports = (gulp, plugins, utilities) => {
-    return () => gulp.src(`${utilities.paths.BUILD_FOLDER}/**/*`, {read: false}).pipe(plugins.clean());
+    return () => gulp.src(utilities.paths.BUILD_FOLDER, {read: false}).pipe(plugins.clean());
 };
