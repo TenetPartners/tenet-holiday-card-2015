43e577d509015ed582d088c402b922aa7d71cf33
Ernie Bello <ernie@ern.me>
2015-12-18 14:29:11 -0500

    added new videos and background image

--- tasks/hash.js
+++ tasks/hash.js
@@ -8,6 +8,8 @@ module.exports = (gulp, plugins, utilities) => {
       `${utilities.paths.BUILD_FOLDER}/**/*.gif`,
       `${utilities.paths.BUILD_FOLDER}/**/*.ico`,
       `${utilities.paths.BUILD_FOLDER}/**/*.svg`,
+      `${utilities.paths.BUILD_FOLDER}/**/*.mp4`,
+      `${utilities.paths.BUILD_FOLDER}/**/*.webm`
     ])
       .pipe(plugins.clean()) // delete original files
       .pipe(plugins.rev())
